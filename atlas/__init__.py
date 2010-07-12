833008f8e6e9aa17148945107a87845df4eb9803
micrypt <micrypt@micrypt-eeepc.(none)>
2010-07-12 05:11:18 +0100

    Initial version - functional

---
+++ atlas/__init__.py
@@ -0,0 +1,16 @@
+# Atlas client library
+# Copyright 2010 - Seyi Ogunyemi
+# See LICENSE for details
+
+"""
+atlas Twitter API library
+"""
+__version__ = '0.1'
+__author__ = 'Seyi Ogunyemi'
+__license__ = 'MIT'
+
+from atlas.error import AtlasError
+from atlas.api import API
+
+# Global, unauthenticated instance of API
+api = API()
