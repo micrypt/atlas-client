5a04e91bbb37273beb20a224f61ff85ca50eca13
Seyi Ogunyemi <micrypt@gmail.com>
2013-11-29 15:47:59 +0000

    api: create endpoints dynamically.

--- atlas/api.py
+++ atlas/api.py
@@ -1,49 +1,50 @@
 # Atlas client library
-# Copyright 2010 - Seyi Ogunyemi
+# Copyright 2010-2013 - Seyi Ogunyemi
 # See LICENSE for details
 
 import urllib
+from types import MethodType
+
 from atlas.error import AtlasError
 from atlas.utils import *
 
-ITEMS_URL = 'http://atlasapi.org/2.0/items.json'
-BRANDS_URL = 'http://atlasapi.org/2.0/brands.json'
-PLAYLISTS_URL = 'http://atlasapi.org/2.0/playlists.json'
+BASE_URL = 'https://atlas.metabroadcast.com/3.0/%s.json'
 
 class API(object):
     """Atlas API"""
 
-    def items(self, *args, **kw):
-        self.response = call(ITEMS_URL, **kw)
-        return self.response
+    def find(self):
+        print 'found'
 
-    def brands(self, *args, **kw):
-        self.response = call(BRANDS_URL, **kw)
-        return self.response
+    def __getattr__(self, name):
+        try:
+            fn = super(API).__getattr__(name)
+        except AttributeError:
+            fn = makeFunc(name)
+            setattr(self, name, MethodType(fn, self))
+        return fn
+        
+def makeFunc(name):
+    def call(self, **kw):
+        """
+        Makes a call to Atlas
 
-    def playlists(self, *args, **kw):
-        self.response = call(PLAYLISTS_URL, **kw)
-        return self.response
-
-def call(url, **kw):
-    """
-    Makes a call to Atlas
-
-    url = Base url for Atlas queries
-    **kw = Query string arguments to be appended to the base URL
-    """
-    json = import_simplejson()
-    if kw:
-        url = url + '?' + urllib.urlencode(kw)
-    print url
-    try:
-        response =  urllib.urlopen(url)
-    except:
-        raise AtlasError("Atlas API IO error")
-    mime_type = response.info().type
-    if (response and mime_type.startswith('application/') and mime_type.endswith('json')):
-        result = json.load(response)
-        return result
-    else:
-        return None
-
+        name = Endpoint for Atlas queries
+        **kw = Query string arguments to be appended to the base URL
+        """
+        url = BASE_URL % name
+        json = import_simplejson()
+        if kw:
+            url = url + '?' + urllib.urlencode(kw)
+        try:
+            response =  urllib.urlopen(url)
+        except:
+            raise AtlasError("Atlas API IO error")
+        mime_type = response.info().type
+        if (response and mime_type.startswith('application/') and mime_type.endswith('json')):
+            result = json.load(response)
+            return result
+        else:
+            return None
+        
+    return call
