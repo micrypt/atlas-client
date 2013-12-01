d9a1533f726d613d10c7a6d2afab02bdf32ff1a5
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-01 15:51:35 +0000

    setup: bump version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="atlas-client",
-      version="0.1",
+      version="0.2",
       description="An Atlas library for python",
       license="MIT",
       author="Seyi Ogunyemi",
