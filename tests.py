333568947fd3ce924bcd9ad1749215d57d118465
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-20 00:30:51 +0000

    test: remove broken tests due to atlas restructuring.

--- tests.py
+++ tests.py
@@ -12,12 +12,6 @@ class AtlasAPITests(unittest.TestCase):
     def testcontent(self):
         self.api.content(uri='http://www.bbc.co.uk/programmes/b007rsj5', annotations='description,brand_summary,series_summary')
 
-    def testbrands(self):
-        self.api.brands(title='east', availableCountries='uk', publisher='bbc.co.uk')
-
-    def testplaylists(self):
-        self.api.playlists(publisher='bbc.co.uk', limit=5)
-
 if __name__ == '__main__':
 
     unittest.main()
