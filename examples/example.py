c1f8707e15e90e9635d124025db47df788bd25db
Seyi Ogunyemi <micrypt@gmail.com>
2014-03-08 19:48:27 +0000

    examples: unify quote marks.

--- examples/example.py
+++ examples/example.py
@@ -1,18 +1,19 @@
 from atlas.api import API
 import pprint
 
-"""API examples"""
+'''API examples'''
 
 atlas_client = API()
 
-"""
+'''
 Inadvertently discovered that there are a TON of results when the publisher is set to  the BBC 
 
 Needless to say, remember to set a limit when uncertain how many results to expect.
-"""
+'''
+
 items = atlas_client.playlists(publisher='bbc.co.uk', limit='2')
 
-items_ = atlas_client.schedule(from_="now", to="now.plus.24h", channel_id="cbbh", publisher='bbc.co.uk')
+items_ = atlas_client.schedule(from_='now', to='now.plus.24h', channel_id='cbbh', publisher='bbc.co.uk')
 
 pprint.pprint(items)
 pprint.pprint(items_)
