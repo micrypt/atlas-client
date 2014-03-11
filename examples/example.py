e147edaccf99defa162482ed40dbf9e35da9b605
Seyi Ogunyemi <micrypt@gmail.com>
2014-03-11 00:05:48 +0000

    examples: remove outdated comment.

--- examples/example.py
+++ examples/example.py
@@ -5,12 +5,6 @@ import pprint
 
 atlas_client = API()
 
-'''
-Inadvertently discovered that there are a TON of results when the publisher is set to  the BBC 
-
-Needless to say, remember to set a limit when uncertain how many results to expect.
-'''
-
 items = atlas_client.schedule(from_='now', to='now.plus.24h', channel_id='cbbh', publisher='bbc.co.uk')
 
 pprint.pprint(items)
