a0c658be0c13f7ca2c4946949bfd72bcc2bc46eb
Seyi Ogunyemi <micrypt@gmail.com>
2014-03-14 21:59:25 +0000

    examples: explain what the example is attempting to do.

--- examples/example.py
+++ examples/example.py
@@ -5,6 +5,8 @@ import pprint
 
 atlas_client = API()
 
+'''Fetch the CBBC schedule for the next 24 hours'''
+
 items = atlas_client.schedule(from_='now', to='now.plus.24h', channel_id='cbbh', publisher='bbc.co.uk')
 
 pprint.pprint(items)
